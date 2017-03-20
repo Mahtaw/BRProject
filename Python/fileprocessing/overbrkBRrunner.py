b6676215ed117759cfa2c8e584ce8a2d37a54fad
mahtaw <mggm6@mst.edu>
2017-03-20 18:33:13 -0500

    got rid of badPaths

--- Python/fileprocessing/overbrkBRrunner.py
+++ Python/fileprocessing/overbrkBRrunner.py
@@ -14,13 +14,12 @@ flowRate=1 #Sets the number for flowrate that will be passed to Matlab
 
 
 cmd="addpath('R:\Research\GeologicalScience\sdm\Source\Matlab');" #adds the path where matlab can find its code
-badPaths=[] #a list of paths that are not valid
+
 for i in folders:
     if os.path.exists(i+'\\area13.out'): #checks of the .out files have been made
         #if not os.path.exists(i+'\\brk.csv'): #if the brk has already been made then skip
             cmd+="overbrkBR(false,%f,%s);"%(flowRate,"'"+i+"'") #Adds the matlab command to the cmd string
-    else:
-        badPaths.append(i)
+
 print("Badpaths: ",badPaths)
 #cmd+='quit;'
 print('starting matlab')
